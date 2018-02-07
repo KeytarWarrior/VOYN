bebfa73b877d1403e4eb165c739c1735fab3bc1e
Stoyan Stoyanov <stirringstorm@gmail.com>
2018-02-07 21:38:51 +0000

    AI Foundation + Respawn Tweaks
    Imported Rudimentary AI Base and added respawn effects + sounds

---
+++ HonoursProj/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+	public static GameMaster gm;
+
+	void Start() {
+		if (gm == null) {
+			gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+		}	
+	}
+
+	public Transform playerPrefab;
+	public Transform spawnPoint;
+	public GameObject spawnPrefab;
+
+	public IEnumerator RespawnPlayer() {
+		GetComponent<AudioSource>().Play();
+		yield return new WaitForSeconds(3f);
+
+		Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+		GameObject clone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation);
+		Destroy(clone, 3f);
+	}
+
+	public static void KillPlayer(Player player) {
+		Destroy(player.gameObject);
+		gm.StartCoroutine(gm.RespawnPlayer());
+	}
+}
