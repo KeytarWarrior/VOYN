ea539f2bdbb3bed0c637511bf6acfa3014edbf95
Stoyan Stoyanov <stirringstorm@gmail.com>
2018-02-15 14:18:49 +0000

    Enemy Health Bar Setup
    Setup highly modular system for enemy health and status effects.
    
    Spent extra time making sure it is scalable. Had to learn about getters and setters.

--- HonoursProj/Assets/Scripts/Enemy.cs
+++ HonoursProj/Assets/Scripts/Enemy.cs
@@ -6,15 +6,42 @@ public class Enemy : MonoBehaviour {
 
 	[System.Serializable]
 	public class EnemyStats {
-		public int Health = 100;
+		public int maxHealth = 100;
+
+		private int _curHealth;
+		public int CurHealth {
+			get { return _curHealth; }
+			set { _curHealth = Mathf.Clamp (value, 0, maxHealth); }
+		}
+
+		public void Init() {
+			CurHealth = maxHealth;
+		}
 	}
 
 	public EnemyStats stats = new EnemyStats();
 
+	// Headers just write text in the editor
+	[Header("Optional: ")]
+	[SerializeField]
+	private StatusIndicator statusIndicator;
+
+	private void Start() {
+		stats.Init();
+
+		if (statusIndicator != null) {
+			statusIndicator.SetHealth(stats.CurHealth, stats.maxHealth);
+		}
+	}
+
 	public void DamageEnemy(int damage) {
-		stats.Health -= damage;
-		if (stats.Health <= 0) {
+		stats.CurHealth -= damage;
+		if (stats.CurHealth <= 0) {
 			GameMaster.KillEnemy(this);
+		}
+
+		if (statusIndicator != null) {
+			statusIndicator.SetHealth(stats.CurHealth, stats.maxHealth);
 		}
 	}
 }
---
+++ HonoursProj/Assets/StatusIndicator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusIndicator : MonoBehaviour {
+
+	[SerializeField] // Makes it show up in editor, even though it is private
+	private RectTransform healthBarRect;
+	[SerializeField]
+	private Text healthText;
+
+	private void Start() {
+		if (healthBarRect == null) {
+			Debug.LogError("STATUS INDICATOR: No health bar object referenced!");
+		}
+		if (healthText == null) {
+			Debug.LogError("STATUS INDICATOR: No health text object referenced!");
+		}
+	}
+
+	// Underscore makes it epxlicit via naming that a variable is private
+	public void SetHealth(int _cur, int _max) {
+		float _value = (float)_cur / _max;
+
+		healthBarRect.localScale = new Vector3(_value, healthBarRect.localScale.y, healthBarRect.localScale.z);
+		healthText.text = _cur + "/" + _max + " HP";
+		//TODO: Grab image component and use a gradient value that you have created and lerp through it
+	}
+}
