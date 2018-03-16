f152b267dd504313eada57d0840c09aff30d22bc
Stoyan Stoyanov <stirringstorm@gmail.com>
2018-03-16 00:29:36 +0000

    Upgrade UI 0.2.5
    Minor setup for new dedicated Stats class, indpendednt of palyer object.
    
    Offers better scalability

--- HonoursProj/Assets/Scripts/Player.cs
+++ HonoursProj/Assets/Scripts/Player.cs
@@ -5,23 +5,6 @@ using UnityEngine;
 [RequireComponent(typeof(Platformer2DUserControl))]
 public class Player : MonoBehaviour {
 
-	[System.Serializable]
-	public class PlayerStats {
-		public int maxHealth = 100;
-
-		private int _curHealth;
-		public int curHealth {
-			get { return _curHealth; }
-			set { _curHealth = Mathf.Clamp(value, 0, maxHealth); }
-		}
-
-		public void Init() {
-			curHealth = maxHealth;
-		}
-	}
-
-	public PlayerStats stats = new PlayerStats();
-
 	// Variable for falling out of bounds
 	public int fallBoundary = -20;
 
@@ -34,8 +17,12 @@ public class Player : MonoBehaviour {
 	[SerializeField]
 	private StatusIndicator statusIndicator;
 
+	// Casting the instance into a local variable so it looks neater
+	private PlayerStats stats;
+
 	private void Start() {
-		stats.Init();
+
+		stats = PlayerStats.instance;
 
 		if (statusIndicator == null) {
 			Debug.LogError("No status indicator referenced on Player");
